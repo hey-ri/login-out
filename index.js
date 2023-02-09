0cfef8738279017469799dde434e1f32230d2c22
hey-ri <kwonhr01@naver.com>
2023-02-09 17:36:08 +0900

    make a router for join/sign up

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//application/x-www-form-urlencode
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
@@ -16,3 +16,24 @@ mongoose
   .connect('mongodb+srv://hyeri:qlqjs1234@boilerplate.8kofbi0.mongodb.net/?retryWrites=true&w=majority', {})
   .then(() => console.log('mongooDB connected'))
   .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//회원가입을 위한 router만들기
+app.post('/register', (req, res) => {
+  //회원가입할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  }); //mongo db에서 들어오는 메소드 save
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
