b33e91347dc0a0b2452d16b07fff5a077c1436c8
hey-ri <kwonhr01@naver.com>
2023-02-09 18:29:21 +0900

    password encrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -10,7 +12,7 @@ const userSchema = mongoose.Schema({
     // 공백 삭제기능 trim
     unique: 1,
   },
-  Password: {
+  password: {
     type: String,
     maxlength: 50,
   },
@@ -30,6 +32,26 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre('save', function (next) {
+  var user = this;
+  //password가 수정 되었을 때 밑에 로직 수행
+  if (user.isModified('password')) {
+    //mongoose에 있는 pre()메소드를 활용해서 index.js에 있는 save 전에 pre()를 하라는 의미이다.
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
