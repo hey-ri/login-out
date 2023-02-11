ebd9efc951dc1be73ff0a1290965ec84aa2a30fd
hey-ri <kwonhr01@naver.com>
2023-02-12 01:34:10 +0900

    connect server and client by axios and proxy

--- server/index.js
+++ server/index.js
@@ -25,6 +25,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요');
 });
 
+app.get('/api/hello', (req, res) => {
+  res.send('안녕');
+});
+
 //회원가입을 위한 router만들기
 app.post('/api/users/register', (req, res) => {
   //회원가입할 때 필요한 정보들을 client에서 가져오면
