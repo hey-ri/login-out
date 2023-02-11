080a1de62d1cea80485b01d8d2c23e876a005fe9
hey-ri <kwonhr01@naver.com>
2023-02-12 01:31:47 +0900

    react router setting and file rename for error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
