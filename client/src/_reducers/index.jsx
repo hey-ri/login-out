d7f5d64aa5287b56e81bdc1219ec5d1e7c0f06c2
hey-ri <kwonhr01@naver.com>
2023-02-13 11:52:21 +0900

    Commented out by generating an error

--- client/src/_reducers/index.jsx
+++ client/src/_reducers/index.jsx
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-import user from './user_reducer';
+// import user from './user_reducer';
 
 //store에서 여러개의 reducer를 사용 할 수 있는데, state가 변하면서(state는 여러개의 묶음으로 있을 수 있음) 변한 값을 return 해 주는 것이 reducer인데, 그 여러개의 state 묶음, 즉 reducer를 rootReducer로 합쳐주는 것이 conbineReducers의 하는 역할이다.
 const rootReducer = combineReducers({
-  user,
+  // user,
 });
 
 export default rootReducer;
--- client/src/_reducers/user_reducer.jsx
+++ client/src/_reducers/user_reducer.jsx
@@ -1,1 +1,2 @@
 //로그인, 레지스터에 대한 기능을 reducer로 만들어주는 곳
+// export default user;
