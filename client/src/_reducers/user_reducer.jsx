aaa8ce8c0c5e0311728772bfb6c4bcb8785f4fe7
hey-ri <kwonhr01@naver.com>
2023-02-13 13:11:28 +0900

    Find user and connect

--- client/src/_reducers/user_reducer.jsx
+++ client/src/_reducers/user_reducer.jsx
@@ -1,2 +1,14 @@
 //로그인, 레지스터에 대한 기능을 reducer로 만들어주는 곳
-// export default user;
+import { LOGIN_USER } from '../_actions/types';
+const initialState = null;
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LOGIN_USER: {
+      return { ...state, loginSuccess: action.payload };
+      break;
+    }
+    default: {
+      return state;
+    }
+  }
+}
