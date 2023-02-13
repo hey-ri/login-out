815ce81e22edfbf6789b4fa368f2c064096a85ad
hey-ri <kwonhr01@naver.com>
2023-02-13 11:57:15 +0900

    create the login page

--- client/src/components/views/LoginPage/LoginPage.jsx
+++ client/src/components/views/LoginPage/LoginPage.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 function Login() {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onEmailHandler = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    let body = {
+      email: email,
+      password: password,
+    };
+
+    Axios.post('/api/users/login', body).then((response) => {});
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler} style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="" value={email} onChange={onEmailHandler} />
+        <label htmlFor="pass">Password</label>
+        <input type="password" name="" id="pass" value={password} onChange={onPasswordHandler} />
+
+        <button>login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
