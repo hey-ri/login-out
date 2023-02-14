02ff15bd68b23bf943eedeeaa5329f01eab47a67
hey-ri <kwonhr01@naver.com>
2023-02-14 11:01:27 +0900

    delete braces{}

--- client/src/_reducers/user_reducer.jsx
+++ client/src/_reducers/user_reducer.jsx
@@ -3,12 +3,11 @@ import { LOGIN_USER } from '../_actions/types';
 const initialState = null;
 export default function (state = initialState, action) {
   switch (action.type) {
-    case LOGIN_USER: {
+    case LOGIN_USER:
       return { ...state, loginSuccess: action.payload };
       break;
-    }
-    default: {
+
+    default:
       return state;
-    }
   }
 }
