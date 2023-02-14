3a549ec6f6eb29ec499384b9820b36cf2e84cd21
hey-ri <kwonhr01@naver.com>
2023-02-14 20:10:00 +0900

    create auth logic

--- client/src/_actions/user_action.jsx
+++ client/src/_actions/user_action.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER, REGISTER_USER } from './types';
+import { AUTH_USER, LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataToSubmit) {
   const request = axios.post('/api/users/login', dataToSubmit).then((response) => response.data);
@@ -11,14 +11,20 @@ export function loginUser(dataToSubmit) {
   };
 }
 export function registerUser(dataToSubmit) {
-  const request = axios
-    .post('/api/users/register', dataToSubmit)
-    .then((response) => response.data)
-    .then(console.log('서버에 요청 했습니다.'));
+  const request = axios.post('/api/users/register', dataToSubmit).then((response) => response.data);
 
   //return을 해서 reducer에 보내고, 전의 state와 현재 state를 다음 state로 만들어 주기 때문에 넘겨 주어야 한다.
   return {
     type: REGISTER_USER,
+    payload: request,
+  };
+}
+export function auth() {
+  const request = axios.get('/api/uses/auth').then((response) => response.data);
+
+  //return을 해서 reducer에 보내고, 전의 state와 현재 state를 다음 state로 만들어 주기 때문에 넘겨 주어야 한다.
+  return {
+    type: AUTH_USER,
     payload: request,
   };
 }
--- client/src/_reducers/user_reducer.jsx
+++ client/src/_reducers/user_reducer.jsx
@@ -9,6 +9,9 @@ export default function (state = {}, action) {
     case REGISTER_USER:
       return { ...state, register: action.payload };
 
+    case AUTH_USER:
+      return { ...state, userData: action.payload };
+
     default:
       return { ...state };
   }
