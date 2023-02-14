87494b4c670bb6755095750739aeac9a6bd1fd6e
hey-ri <kwonhr01@naver.com>
2023-02-14 14:52:43 +0900

    create the logout logic

--- client/src/components/views/LandingPage/LandingPage.jsx
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -1,16 +1,30 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+  const navigate = useNavigate();
   useEffect(() => {
     axios.get('/api/hello').then((response) => {
       console.log(response);
     });
   }, []);
 
+  const logOutHandler = () => {
+    axios.get('/api/users/logout').then((res) => {
+      console.log(res.data);
+      if (res.data.success) {
+        navigate('/login');
+      } else {
+        alert('로그아웃하는데 실패했습니다.');
+      }
+    });
+  };
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
       <h2>시작페이지</h2>
+      <button onClick={logOutHandler}>log out</button>
     </div>
   );
 }
