6769526f23a67649db7db02e335245912ae9c430
hey-ri <kwonhr01@naver.com>
2023-02-15 19:24:29 +0900

    Divide the auth branch

--- client/src/hoc/auth.jsx
+++ client/src/hoc/auth.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { auth } from '../_actions/user_action';
+import { useNavigate } from 'react-router-dom';
 
 export default function (SpecificComponent, option, adminRoute = null) {
   //option
@@ -10,10 +11,26 @@ export default function (SpecificComponent, option, adminRoute = null) {
   //true = admin만 출입 null= 아무나 출입가능
   function AuthenticationCheck(props) {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
       dispatch(auth()).then((res) => {
         console.log(res);
+        if (!res.payload.isAuth) {
+          //로그인하지 않은 상태
+          if (option) {
+            navigate('/login');
+          }
+        } else {
+          //로그인한 상태
+          if (adminRoute && !res.payload.isAdmin) {
+            navigate('/');
+          } else {
+            if (option == false) {
+              navigate('/');
+            }
+          }
+        }
       });
     }, []);
     return <SpecificComponent />;
